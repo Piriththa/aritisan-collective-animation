cf16317e74fd04e6827b87b666f44fd441c63c95
Piriththa Mohanathas <piriththamohan@gmail.com>
2026-01-03 19:16:24 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import HeroSection from "./components/HeroSection";
+import About from "./components/About";
+import CraftTimeline from "./components/CraftTimeline";
+import Contact from "./components/Contact";
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <About />
+      <CraftTimeline />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
